99c66b57d11dc27aba2c3ca7f1778224796bc983
Hammadi-32 <hmade3696@gmail.com>
2025-05-31 23:39:01 +0300

    add new component for school and governorates and governorate-details

---
+++ src/app/components/Governorates/components/component/governorates.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { GovernorateServices } from '../../services/governorate.services';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { Governorates } from '../../models/governorate.model';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-governorates',
+  imports:
+    [
+      CommonModule,
+      RouterModule
+    ],
+  templateUrl: './governorates.component.html',
+  styleUrl: './governorates.component.scss',
+  standalone: true
+})
+export class GovernoratesComponent implements OnInit {
+
+  governorates: Governorates[] = [];
+
+  constructor(private governorateServices: GovernorateServices) { }
+  
+  ngOnInit(): void {
+    this.getAllGovernorates()
+  }
+
+  getAllGovernorates() {
+    this.governorateServices.getListGovernorates().subscribe(res => {
+      this.governorates = res;
+    })
+  }
+}
