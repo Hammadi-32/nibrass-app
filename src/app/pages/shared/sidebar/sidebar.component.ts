7f19f401d891126e3fc92e54625cf57ca10aa123
Hamza Al Saado <Hsaado@beamra.com>
2025-08-31 17:20:47 +0300

    show profile icon if user logged in

--- src/app/pages/shared/sidebar/sidebar.component.ts
+++ src/app/pages/shared/sidebar/sidebar.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { getUserInfo } from '../../../functions/getUserInfo';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
@@ -14,23 +15,32 @@ export class SidebarComponent implements OnInit {
   
   @Output() toggleSideBar = new EventEmitter<boolean>()
   sidebarClosed: boolean = true;
-  userRole: string = 'Administrator';
+  userRole: string = '11';
   userInfo: any;
+  isLoggedin: boolean = false;
+
+  constructor(private router: Router){}
 
   ngOnInit(): void {
-    this.userInfo = getUserInfo();
-    this.userRole = this.userInfo.role
+    this.router.events.pipe(filter(e => e instanceof NavigationEnd))
+    .subscribe(() => {
+      const raw = localStorage.getItem('user-Info');
+      const user = raw ? JSON.parse(raw) : null;
+      console.log(raw)
+      this.isLoggedin = !!user;
+      this.userRole   = user?.role ?? '01';
+    });
   }
 
   menuItems = [
-    { icon: 'account_circle', label: 'الملف الشخصي', route: '/user-profile' },
-    { icon: 'home', label: 'الرئيسية', route: '/home' },
-    { icon: 'school', label: 'المدارس', route: '/schools' },
-    { icon: 'account_balance', label: 'المحافظات', route: '/governorates' },
-    // { icon: 'location_city', label: 'المدن', route: '/cities' },
-    // { icon: 'insert_chart', label: 'التقارير', route: '/reports' },
+    { icon: 'account_circle',   label: 'الملف الشخصي',            route: '/user-profile', onlyUser: true },
+    { icon: 'home',             label: 'الرئيسية',                route: '/home' },
+    { icon: 'school',           label: 'المدارس',                 route: '/schools' },
+    { icon: 'account_balance',  label: 'المحافظات',               route: '/governorates' },
     { icon: 'hourglass_bottom', label: 'مدارس بانتظار المراجعة', route: '/pending-schools', onlyAdmin: true },
-    { icon: 'info', label: 'من نحن', route: '/about' },
+    { icon: 'info',             label: 'من نحن',                  route: '/about' },
+    // { icon: 'location_city', label: 'المدن',                   route: '/cities' },
+    // { icon: 'insert_chart',  label: 'التقارير',                route: '/reports' },
   ];
 
   sideBar() {
